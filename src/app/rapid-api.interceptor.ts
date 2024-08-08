52ca389f7360e53f61cc67aed8fb56664adfe1d4
amt-richard-dadzie <r.dadzie@amalitech.com>
2024-08-08 16:59:42 +0000

    refactor: create interceptor for api headers

---
+++ src/app/rapid-api.interceptor.ts
@@ -0,0 +1,16 @@
+import { HttpInterceptorFn } from '@angular/common/http';
+import { environment } from '../environments/environment.development';
+
+export const rapidApiInterceptor: HttpInterceptorFn = (req, next) => {
+  if (req.url.startsWith(environment.apiUrl)) {
+    const modifiedReq = req.clone({
+      setHeaders: {
+        'x-rapidapi-key': environment.rapidApiKey,
+        'x-rapidapi-host': 'instagram-scraper-api2.p.rapidapi.com',
+      },
+    });
+
+    return next(modifiedReq);
+  }
+  return next(req);
+};
